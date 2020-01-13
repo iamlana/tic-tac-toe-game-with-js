9cd61102a201bf47105c9fda5872b8b72b1a07be
Anna Lukianova <ann.lookyanova@gmail.com>
2020-01-13 22:54:03 +0100

    JS: Basic AI & Winner Box

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let origBoard;
-const huPlayer = '0';
+const huPlayer = 'O';
 const aiPlayer = 'X';
 const winCombination = [
     [0, 1, 2],
@@ -25,10 +25,69 @@ function startGame() {
 }
 
 function turnClick(square) {
-    turn(square.target.id, huPlayer)
+    if (typeof origBoard[square.target.id] == 'number') {
+        turn(square.target.id, huPlayer)
+        if (!checkTie()) turn(bestSpot(), aiPlayer);
+    }
+
 }
 
 function turn(squareId, player) {
     origBoard[squareId] = player;
     document.getElementById(squareId).innerText = player;
+    let gameWon = checkWin(origBoard, player)
+    if (gameWon) gameOver(gameWon)
+}
+
+function checkWin(board, player) {
+    let plays = board.reduce((a, e, i) =>
+        (e === player) ? a.concat(i) : a,
+        []);
+    let gameWon = null;
+    for (let [index, win] of winCombination.entries()) {
+        if (win.every(elem => plays.indexOf(elem) > -1)) {
+            gameWon = {
+                index: index,
+                player: player
+            };
+            break;
+        }
+    }
+    return gameWon;
+}
+
+function gameOver(gameWon) {
+    for (let index of winCombination[gameWon.index]) {
+        document.getElementById(index).style.backgroundColor =
+            gameWon.player == huPlayer ? "blue" : "red";
+    }
+    for (var i = 0; i < cells.length; i++) {
+        cells[i].removeEventListener('click', turnClick, false)
+    }
+    declareWinner(gameWon.player == huPlayer ? "you win" : "you lose")
+}
+
+function declareWinner(who) {
+    document.querySelector(".endgame").style.display = "block";
+    document.querySelector(".endgame .text").innerText = who;
+}
+
+function emptySquares() {
+    return origBoard.filter(s => typeof s == 'number');
+}
+
+function bestSpot() {
+    return emptySquares()[0];
+}
+
+function checkTie() {
+    if (emptySquares().length == 0) {
+        for (var i = 0; i < cells.length; i++) {
+            cells[i].style.backgroundColor = "green";
+            cells[i].removeEventListener('click', turnClick, false);
+        }
+        declareWinner("Tie Game")
+        return true;
+    }
+    return false;
 }
