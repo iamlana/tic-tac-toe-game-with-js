f36764e4a1195b3f767937006b549cba86ba6138
Anna Lukianova <ann.lookyanova@gmail.com>
2020-01-13 21:53:02 +0100

    Creating basic setup

---
+++ main.js
@@ -0,0 +1,34 @@
+let origBoard;
+const huPlayer = '0';
+const aiPlayer = 'X';
+const winCombination = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+const cells = document.querySelectorAll('.cell');
+
+function startGame() {
+    document.querySelector(".endgame").style.display = "none";
+    origBoard = Array.from(Array(9).keys());
+    for (var i = 0; i < cells.length; i++) {
+        cells[i].inneText = '';
+        cells[i].style.removeProperty('background-color');
+        cells[i].addEventListener('click', turnClick, false);
+    }
+
+}
+
+function turnClick(square) {
+    turn(square.target.id, huPlayer)
+}
+
+function turn(squareId, player) {
+    origBoard[squareId] = player;
+    document.getElementById(squareId).innerText = player;
+}
