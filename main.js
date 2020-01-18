2110a57bb7a528d40c333a69bff49fbd7d6e650c
Anna Lukianova <ann.lookyanova@gmail.com>
2020-01-18 21:48:18 +0100

    Fixing errors

--- main.js
+++ main.js
@@ -12,12 +12,13 @@ const winCombination = [
     [2, 4, 6]
 ]
 const cells = document.querySelectorAll('.cell');
+startGame();
 
 function startGame() {
     document.querySelector(".endgame").style.display = "none";
     origBoard = Array.from(Array(9).keys());
     for (var i = 0; i < cells.length; i++) {
-        cells[i].inneText = '';
+        cells[i].innerText = '';
         cells[i].style.removeProperty('background-color');
         cells[i].addEventListener('click', turnClick, false);
     }
@@ -77,7 +78,7 @@ function emptySquares() {
 }
 
 function bestSpot() {
-    return emptySquares()[0];
+    return minimax(origBoard, aiPlayer).index;
 }
 
 function checkTie() {
@@ -90,4 +91,55 @@ function checkTie() {
         return true;
     }
     return false;
+}
+
+function minimax(newBoard, player) {
+    var availSpots = emptySquares(newBoard);
+    if (checkWin(newBoard, player)) {
+        return {
+            score: -10
+        };
+    } else if (checkWin(newBoard, aiPlayer)) {
+        return {
+            score: 20
+        };
+    } else if (availSpots.length === 0) {
+        return {
+            score: 0
+        };
+    }
+    var moves = [];
+    for (var i = 0; i < availSpots.length; i++) {
+        var move = {};
+        move.index = newBoard[availSpots[i]];
+        newBoard[availSpots[i]] = player;
+        if (player == aiPlayer) {
+            var result = minimax(newBoard, huPlayer);
+            move.score = result.score;
+        } else {
+            var result = minimax(newBoard, aiPlayer);
+            move.score = result.score;
+        }
+        newBoard[availSpots[i]] = move.index;
+        moves.push(move);
+    }
+    var bestMove;
+    if (player === aiPlayer) {
+        var bestScore = -10000;
+        for (var i = 0; i < moves.length; i++) {
+            if (moves[i].score > bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+        }
+    } else {
+        var bestScore = 10000;
+        for (var i = 0; i < moves.length; i++) {
+            if (moves[i].score < bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+        }
+    }
+    return moves[bestMove];
 }
